0b62c15beab386b55636182886ecdfc3d2e3ab0c
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-13 21:00:39 +0200

    chore: apply async handler

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,14 +1,15 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 
 import { isAuthenticated } from '../../middlewares'
 import { authController } from '../../controllers'
 
 const router = express.Router()
 
-router.post('/register', authController.register)
+router.post('/register', asyncHandler(authController.register))
 
-router.post('/login', authController.login)
+router.post('/login', asyncHandler(authController.login))
 
-router.post('/logout', isAuthenticated, authController.logout)
+router.post('/logout', isAuthenticated, asyncHandler(authController.logout))
 
 export { router as authRoutes }
