41bdc72c1cf8bd5f3961c9e670d3f5492ede3ae4
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-13 19:54:38 +0200

    chore: add statics and methods to user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,15 +1,25 @@
 import { Schema, model, Model } from 'mongoose'
+import bcrypt from 'bcryptjs'
+
 import { userTypes } from '../types'
 
-const userSchema = new Schema(
+interface IUser extends Model<userTypes.UserDocument> {
+  doesEmailExist: (email: string) => Promise<boolean>
+}
+
+const userSchema: Schema<userTypes.UserDocument> = new Schema(
   {
     email: {
       type: Schema.Types.String,
       required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
     },
     password: {
       type: Schema.Types.String,
       required: true,
+      trim: true,
     },
   },
   {
@@ -17,6 +27,29 @@ const userSchema = new Schema(
   }
 )
 
-const User: Model<userTypes.UserDocument> = model('User', userSchema)
+userSchema.statics.doesEmailExist = async function (
+  email: string
+): Promise<boolean> {
+  const user = await this.findOne({ email })
+  return Boolean(user)
+}
+
+userSchema.methods.doesPasswordMatch = async function (
+  password: string
+): Promise<boolean> {
+  const user = this
+  return bcrypt.compare(password, user.password)
+}
+
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+const User: IUser = model<userTypes.UserDocument, IUser>('User', userSchema)
 
 export { User }
