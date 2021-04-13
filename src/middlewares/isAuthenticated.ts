1e874233b73bd16eabc292ead6039fcaa2debcd3
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-13 21:00:16 +0200

    chore: apply http-errors lib function

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,30 +1,27 @@
 import { Request, Response, NextFunction } from 'express'
 import passport from 'passport'
+import createHttpError from 'http-errors'
 
 import { findToken } from '../services/token.service'
 
 function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-  try {
-    passport.authenticate(
-      'jwt',
-      { session: false },
-      async (error, payload, info) => {
-        if (error || !payload || info) {
-          throw new Error(info)
-        }
-
-        const token = await findToken(req.headers.authorization.split(' ')[1])
-
-        if (token) {
-          throw new Error('Token blacklisted')
-        }
-
-        return next()
+  passport.authenticate(
+    'jwt',
+    { session: false },
+    async (error, payload, info) => {
+      if (error || !payload || info) {
+        next(createHttpError(400, error || info))
       }
-    )(req, res, next)
-  } catch (error) {
-    next(error)
-  }
+
+      const token = await findToken(req.headers.authorization.split(' ')[1])
+
+      if (token) {
+        next(createHttpError(401, 'Token blacklisted'))
+      }
+
+      next()
+    }
+  )(req, res, next)
 }
 
 export { isAuthenticated }
