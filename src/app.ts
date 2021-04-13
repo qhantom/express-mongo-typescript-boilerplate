5ddfec701daddac7222ae305b1cae6610cc7d751
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-13 19:52:02 +0200

    chore: apply passport middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import passport from 'passport'
+import { strategy } from './configs'
+
 const app = express()
 
 import { router as routes } from './routes/v1'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+passport.use('jwt', strategy)
 
 // ROUTES
 app.use('/v1', routes)
