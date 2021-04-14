b695292c2661ae1da9e689a60a9c012fd22ebb63
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-14 21:24:17 +0200

    chore: add validation middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { isAuthenticated } from './isAuthenticated'
 export { authLimiter } from './rateLimiter'
+export { validate, registerValidator, loginValidator } from './validation'
---
+++ src/middlewares/validation.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express'
+import { body, validationResult } from 'express-validator'
+
+function registerValidator() {
+  return [
+    body('email').isEmail().normalizeEmail().trim(),
+    body('password').trim().isStrongPassword(),
+  ]
+}
+
+function loginValidator() {
+  return [
+    body('email').isEmail().normalizeEmail().trim(),
+    body('password').trim().isStrongPassword(),
+  ]
+}
+
+function validate(req: Request, res: Response, next: NextFunction) {
+  const errors = validationResult(req)
+
+  if (errors.isEmpty()) {
+    return next()
+  }
+
+  const sanitizedErrors = errors
+    .array()
+    .map((error) => ({ [error.param]: error.msg }))
+
+  return res.status(400).json({ errors: sanitizedErrors })
+}
+
+export { validate, registerValidator, loginValidator }
