38fd369f9e2de60d9c192d80772e67872549365c
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-14 21:24:43 +0200

    chore: add validation middlewares

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,14 +1,28 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 
-import { isAuthenticated } from '../../middlewares'
+import {
+  isAuthenticated,
+  validate,
+  registerValidator,
+  loginValidator,
+} from '../../middlewares'
 import { authController } from '../../controllers'
 
 const router = express.Router()
+router.post(
+  '/register',
+  registerValidator(),
+  validate,
+  asyncHandler(authController.register),
+)
 
-router.post('/register', asyncHandler(authController.register))
-
-router.post('/login', asyncHandler(authController.login))
+router.post(
+  '/login',
+  loginValidator(),
+  validate,
+  asyncHandler(authController.login),
+)
 
 router.post('/logout', isAuthenticated, asyncHandler(authController.logout))
 
