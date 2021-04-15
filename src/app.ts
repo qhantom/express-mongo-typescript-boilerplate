17e2fb31bd522d7776fc376b9f9cca6b1df6ba9b
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-15 18:50:08 +0200

    chore: add logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express'
 import passport from 'passport'
 import createHttpError, { HttpError } from 'http-errors'
+import helmet from 'helmet'
 
-import { config, strategy } from './configs'
+import { config, strategy, successHandler, errorHandler } from './configs'
 
 import { authLimiter } from './middlewares'
 
@@ -10,12 +11,17 @@ import { router as routes } from './routes/v1'
 
 const app = express()
 
+app.use(successHandler)
+app.use(errorHandler)
+
+app.use(helmet())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 passport.use('jwt', strategy)
 
-if (config.environment === 'production') {
+if (config.environment !== 'development') {
   app.use('/v1/auth', authLimiter)
 }
 
@@ -26,13 +32,15 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next(createHttpError(404))
 })
 
-app.use((err: HttpError, req: Request, res: Response) => {
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
   const { status = 500, message, stack } = err
+
+  res.locals.errorMessage = message
 
   res.status(status).json({
     status,
     message,
-    stack: config.environment === 'production' ? null : stack,
+    stack: config.environment === 'development' ? stack : null,
   })
 })
 
