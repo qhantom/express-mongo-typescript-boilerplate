cfbe2288ec3112581af0c2c502d4e136c95f4204
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-15 20:21:50 +0200

    chore: styling changes

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ import { config, logger } from './configs'
 mongoose.connect(
   config.database.URI,
   config.database.options,
-  (err: NativeError) => {
-    if (err) {
-      logger.error(`Database Error: ${err}`)
+  (error: NativeError) => {
+    if (error) {
+      logger.error(`Database Error: ${error}`)
     } else {
       logger.info('Database connected')
 
       app.listen(config.server.port, () => {
-        logger.info(`Server started in port ${config.server.port}`)
+        logger.info(`Server started on port ${config.server.port}`)
       })
     }
   },
