07b43bfbf396f07cfa1f291434beb1a7a79e77da
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-15 18:49:18 +0200

    chore: create morgan http logger for success and error cases

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,2 +1,4 @@
 export { config } from './config'
 export { strategy } from './passport'
+export { logger } from './logger'
+export { errorHandler, successHandler } from './morgan'
---
+++ src/configs/morgan.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import morgan from 'morgan'
+
+import { config, logger } from '.'
+
+morgan.token(
+  'message',
+  (req: Request, res: Response) => res.locals.errorMessage || '',
+)
+
+const getIpFormat = () =>
+  config.environment === 'development' ? '' : ':remote-addr - '
+const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`
+const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`
+
+const successHandler = morgan(successResponseFormat, {
+  skip: (req: Request, res: Response) => res.statusCode >= 400,
+  stream: { write: (message: string) => logger.info(message.trim()) },
+})
+
+const errorHandler = morgan(errorResponseFormat, {
+  skip: (req: Request, res: Response) => res.statusCode < 400,
+  stream: { write: (message: string) => logger.error(message.trim()) },
+})
+
+export { successHandler, errorHandler }
