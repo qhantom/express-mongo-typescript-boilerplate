bb4c99a78f96827ea5cda08f154b6d3fd9d8cd37
Gerrit <gerrit.schroeer@frachtwerk.de>
2021-04-15 18:48:41 +0200

    chore: replace console.log with logger

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import mongoose, { NativeError } from 'mongoose'
 
 import { app } from './app'
 
-import { config } from './configs'
+import { config, logger } from './configs'
 
 mongoose.connect(
   config.database.URI,
   config.database.options,
   (err: NativeError) => {
     if (err) {
-      console.log('Database Error', err)
+      logger.error(`Database Error: ${err}`)
     } else {
-      console.log('Database connected')
+      logger.info('Database connected')
 
       app.listen(config.server.port, () => {
-        console.log(`Server started in port ${config.server.port}`)
+        logger.info(`Server started in port ${config.server.port}`)
       })
     }
   },
