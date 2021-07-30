98ae1cdb00bdb16aaa336e8dea440a4b510ee36f
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2021-07-30 17:10:30 +0200

    fix: omit password in query response

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,43 +1,36 @@
 import createHttpError from 'http-errors'
 
-import { userTypes } from '../types'
-
 import { User } from '../models'
 
-async function getUsers(): Promise<userTypes.UserDocument[]> {
-  const users: userTypes.UserDocument[] = await User.find().select('-password')
+import { UserDocument, User as IUser } from '../types/user.type'
+
+async function getUsers(): Promise<UserDocument[]> {
+  const users: UserDocument[] = await User.find().select('-password')
   return users
 }
 
-async function getUser(id: string): Promise<userTypes.UserDocument> {
-  const user: userTypes.UserDocument = await User.findById(id).select(
-    '-password',
-  )
+async function getUser(id: string): Promise<UserDocument> {
+  const user: UserDocument = await User.findById(id).select('-password')
   return user
 }
 
-async function createUser(
-  body: userTypes.User,
-): Promise<userTypes.UserDocument> {
+async function createUser(body: IUser): Promise<UserDocument> {
   if (await User.doesEmailExist(body.email)) {
     throw createHttpError(400, 'Email already exists')
   }
 
-  const user: userTypes.UserDocument = await User.create(body)
+  const user: UserDocument = await User.create(body)
   return user
 }
 
-async function updateUser(
-  id: string,
-  body: userTypes.User,
-): Promise<userTypes.UserDocument> {
+async function updateUser(id: string, body: IUser): Promise<UserDocument> {
   if (await User.doesEmailExist(body.email)) {
     throw createHttpError(400, 'Email already exists')
   }
 
-  const user: userTypes.UserDocument = await User.findByIdAndUpdate(id, body, {
+  const user: UserDocument = await User.findByIdAndUpdate(id, body, {
     new: true,
-  })
+  }).select('-password')
   return user
 }
 
